283929ca7fa05ffb6a2fe94a734dff4c853ceb72
monika-pal <monika.pal@moglix.com>
2025-04-05 20:58:11 +0530

    new commit for pre prod

--- src/config/home_config.js
+++ src/config/home_config.js
@@ -5,9 +5,9 @@ import { URL } from "./endpoints";
 
 export const HomeData={
     SeoData:{
-        title:'Office Array | One stop shop for all office chairs and desks',
-        description:"Office Array offers a diverse selection of furniture designed to enhance any workspace. From ergonomic chairs that prioritize comfort and support, to stylish desks built for productivity, each piece combines functionality with aesthetic appeal.",
-        keywords:"office chairs, Mesh chair, Desk, Lethrette chair",
+        title:'Modacaso | Trendy Fashion Accessories for Daily Style',
+        description:"Discover stylish and affordable fashion accessories at Modacaso. Shop earrings, handbags, sunglasses & more to complete your perfect look.",
+        keywords:"",
         canonicalUrl:URL,
         url:URL,
         
